05c71088de90c08f60b5cc4850f4c3e59a053750
81245995@qq.com <81245995@qq.com>
2018-05-16 15:26:19 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {Button} from 'antd';
+
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welc2ome to React</h1>
+
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Button type="primary">fky2</Button>
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
