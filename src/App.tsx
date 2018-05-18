c1eeb038dd72f092be5a8077b5e22d0e2a46ffd9
81245995@qq.com <81245995@qq.com>
2018-05-18 16:47:25 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import {Layout,Menu,Icon,Breadcrumb} from 'antd';
+import {Layout,Menu,Breadcrumb} from 'antd';
 
 import './App.css';
-import { BrowserRouter,Route,Link } from 'react-router-dom'
+import { BrowserRouter,Route } from 'react-router-dom'
 // import logo from './logo.svg';
+import Home from './views/Home/';
+import MenuList from './components/MenuList/';
 const {Header,Sider,Content}=Layout;
-const { SubMenu } = Menu;
-const Home = () => (
+// const { SubMenu } = Menu;
+const About = () => (
   <div>
     <h2>Home</h2>
   </div>
 )
+
 class App extends React.Component {
   
   public render() {
@@ -26,39 +29,17 @@ class App extends React.Component {
             defaultSelectedKeys={['2']}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="1">nav 1</Menu.Item>
+            {/* <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item> */}
           </Menu>
         </Header>
 
         <Layout>
           <Sider width={200} style={{ background: '#fff' }}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{ height: '100%', borderRight: 0 }}
-            >
-              <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                <Menu.Item key="1">option1</Menu.Item>
-                <Menu.Item key="2">option2</Menu.Item>
-                <Menu.Item key="3">option3</Menu.Item>
-                <Menu.Item key="4">option4</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                <Menu.Item key="9">option9</Menu.Item>
-                <Menu.Item key="10">option10</Menu.Item>
-                <Menu.Item key="11">option11</Menu.Item>
-                <Menu.Item key="12">option12</Menu.Item>
-              </SubMenu>
-            </Menu>
+            
+            <MenuList/>
+            
           </Sider>
           <Layout style={{ padding: '0 24px 24px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
@@ -66,11 +47,10 @@ class App extends React.Component {
               <Breadcrumb.Item>List</Breadcrumb.Item>
               <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
-            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-              <Link to="/">Home</Link>
-              <Link to="/about">about</Link>
+            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 680 }}>
+              
               <Route exact={true} path="/" component={Home}/>
-              <Route path="/about" component={Home}/>
+              <Route path="/about" component={About}/>
             </Content>
           </Layout>
         </Layout>
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class Home extends React.Component{
+  public render(){
+    return(
+      <h1>fk</h1>
+    )
+  }
+}
+
+export default Home
