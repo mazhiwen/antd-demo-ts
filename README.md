fb2292dfb893c4c3787ba8dd9d50471735f5e56a
81245995@qq.com <81245995@qq.com>
2018-05-24 17:50:37 +0800

    登录ui

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-## 管理系统
+# 管理系统
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
 You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
 
-
+* 基于Create React App ,typescript 配置
+* 执行eject
 
 ## 步骤
+
 ```sh
 yarn
 yarn start
 ```
-
--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,17 +1,58 @@
 import * as React from 'react';
 import './index.less';
-import {Card} from 'antd';
+import {Card, Form, Icon, Input, Button, Checkbox } from 'antd';
+// import { FormComponentProps } from 'antd/lib/form';
 
-class Login extends React.Component{
+class LoginOrigin extends React.Component <any>{
+  public handleSubmit = (e:any) => {
+    e.preventDefault();
+    this.props["form"].validateFields((err:any, values:any) => {
+      if (!err) {
+        window.console.log('Received values of form: ', values);
+      }
+    });
+  }
   public render(){
+    const { getFieldDecorator } = this.props['form'];
     return(
       <div className="login">
-        
-        <Card>登录</Card>
+        <Card>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(
+                <Checkbox>Remember me</Checkbox>
+              )}
+              <a className="login-form-forgot" href="">Forgot password</a>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+              Or <a href="">register now!</a>
+            </Form.Item>
+          </Form>
+
+        </Card>
       </div>
       
     )
   }
 }
+const Login = Form.create()(LoginOrigin);
 
 export default Login
